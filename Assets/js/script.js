93dc44ce506d9b39cada2458e704886123b34387
Stephanie <101684009+schaparro08@users.noreply.github.com>
2022-04-09 21:58:16 -0400

    creation of collumns for weather data

---
+++ Assets/js/script.js
@@ -0,0 +1,52 @@
+// // global variables
+// const searchHistory = [];
+// const weatherApiUrl = "https://api.openweathermap.org/";
+
+// const apikey = "8cd90cfd883306f01cb2ce2e7d9d45b7";
+
+// // DOM Elments References
+// // #search form, #search-input, #today, #forecast, #history
+// let searchForm = document.querySelector("#search-form");
+// let searchInput =
+// let todayBox =
+// let forecastBox =
+// let searchDisplayBox =
+
+
+// // add timezone for day.js
+// dayjs.extend(window.dayjs_plgin_utc);
+// dayjs.extend(window.dayjs_plugin_timezone);
+
+
+
+
+// // function to display search history
+// function displaySearchHistory() {
+//  searchDisplayBox.innerhtml = “”,
+// // }
+
+
+// function weatherData(weatherApiUrl) {
+//     fetch(weatherApiUrl)
+//     .then(function(res) {
+//         return res.json()
+//     })
+//     .then(function(data) {
+//         console.log(data)
+//     })
+// }
+// weatherData(weatherApiUrl);
+// // make a request to the one call and show them how to make api key
+// // fetch coordinates
+// // fetch weather
+// // function renderItems(city, data) {
+// // }
+// // 1. geocoding API for lat and long
+
+
+// function fetchWeather(location) {
+//     let { lat } = location;
+//     let { long } = location;
+//     let cityName = location.name;
+//     let apiurl = ${weatherApiUrl}...;
+// }
