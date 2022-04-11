50b739fd6e83be18b9d8c0a3949eb0db483be95e
Stephanie <101684009+schaparro08@users.noreply.github.com>
2022-04-10 21:18:36 -0400

    Creation of dom traversing variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,31 +1,40 @@
-console.log("#search-button");
 
-// // global variables
-// const searchHistory = [];
-// const weatherApiUrl = "https://api.openweathermap.org/";
+// global variables
+const searchHistory = [];
+const weatherApiUrl = "https://api.openweathermap.org/";
 
-// const apikey = "8cd90cfd883306f01cb2ce2e7d9d45b7";
+const apikey = "8cd90cfd883306f01cb2ce2e7d9d45b7";
 
 // // DOM Elments References
 // // #search form, #search-input, #today, #forecast, #history
-// let searchForm = document.querySelector("#search-form");
-// let searchInput =
-// let todayBox =
-// let forecastBox =
-// let searchDisplayBox =
-
+let searchForm = document.querySelector("#search-form");
+let searchInput = document.querySelector("#search-input");
+console.log(searchInput);
+let todayBox = document.querySelector("#currentWeather");
+let forecastBox = document.querySelector("#forecastCards");
+let recentSearches = document.querySelector("#recentSearches")
+let searchBtn = document.querySelector("#search-button")
 
 // // add timezone for day.js
-// dayjs.extend(window.dayjs_plgin_utc);
-// dayjs.extend(window.dayjs_plugin_timezone);
+dayjs.extend(window.dayjs_plgin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+console.log(searchInput);
+
+// function to display search history
+function displaySearchHistory(event) {
+    preventDefault(event);
+    //get the value of what was being input
+ var cities = searchInput.value;
+ console.log(cities);
+    localStorage.setItem("City Searches", cities);
+ 
+      
+  }
 
 
 
 
-// // function to display search history
-// function displaySearchHistory() {
-//  searchDisplayBox.innerhtml = “”,
-// // }
 
 
 // function weatherData(weatherApiUrl) {
@@ -52,3 +61,6 @@ console.log("#search-button");
 //     let cityName = location.name;
 //     let apiurl = ${weatherApiUrl}...;
 // }
+
+
+searchBtn.addEventListener("click", displaySearchHistory());
