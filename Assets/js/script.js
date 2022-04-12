6370bc9c03b8efb74b4a0d65d97225a16f9872e1
Stephanie <101684009+schaparro08@users.noreply.github.com>
2022-04-11 20:34:07 -0400

    Creation of README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,14 +22,40 @@ dayjs.extend(window.dayjs_plugin_timezone);
 console.log(searchInput);
 
 // function to display search history
-function displaySearchHistory(event) {
-    preventDefault(event);
-    //get the value of what was being input
- var cities = searchInput.value;
- console.log(cities);
-    localStorage.setItem("City Searches", cities);
+function displaySearchHistory() {
+recentSearches.innerHTML= "";
+//Create a for loop that for each search, input is added to the button. Set I equal to 3
+    
  
-      
+  
+
+  }
+
+  // Function to append everything to the search history
+  // Local storage function
+  function appendSearchHistory(){
+    
+    //Local storage set item
+    //json stringify 
+    
+  }
+
+
+  function dayilyWeather () {
+// add code  from slack
+
+  }
+
+
+  function forecasting () {
+
+
+    // for the five day forcast
+    //create element, set attribute
+    // for loop for all 5 days of the week
+
+    //add code from slack 
+
   }
 
 
@@ -37,30 +63,31 @@ function displaySearchHistory(event) {
 
 
 
+
+
 // function weatherData(weatherApiUrl) {
+  // let { lat } = location;
+  //     let { long } = location;
+  //     let cityName = location.name;
+  // comment: use entire api link here
+  //     let apiurl = ${weatherApiUrl}...;
 //     fetch(weatherApiUrl)
 //     .then(function(res) {
 //         return res.json()
 //     })
 //     .then(function(data) {
+  // // function renderItems(city, data) {
+// // }
 //         console.log(data)
 //     })
 // }
-// weatherData(weatherApiUrl);
+
+// function weatherData(weatherApiUrl);
 // // make a request to the one call and show them how to make api key
 // // fetch coordinates
 // // fetch weather
-// // function renderItems(city, data) {
-// // }
+
 // // 1. geocoding API for lat and long
-
-
-// function fetchWeather(location) {
-//     let { lat } = location;
-//     let { long } = location;
-//     let cityName = location.name;
-//     let apiurl = ${weatherApiUrl}...;
-// }
 
 
 searchBtn.addEventListener("click", displaySearchHistory());
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # APIs-Weather-Dashboard
 Weather a=dashboard that uses the Open-weather API
+
+## APIs-Weather-Dashboard homework ucf bootcamp
+
+
+
+## composed by
+Stephanie Chaparro
+
+## project location ##
+[repo link](https://github.com/schaparro08/APIs-Weather-Dashboard)
+[live link](https://schaparro08.github.io/APIs-Weather-Dashboard/)
+
+**contact information**
+schaparro2021@gmail.com
+
+
+Project example
+
+![screenshot](./assets/images/screencapture-file-Users-stephaniechaparro-roldan-Documents-UCF-Homework-APIs-Weather-Dashboard-index-html-2022-04-11-20_29_31.png)
+
+
+
+## Description
+The API Weather Dashboard utilizes the OpenWeather API in order to draw information for a five day forcast and display it on the screen. The application alos saves the four most recent searches and allows the user to refer back to them. The app also incorporates Bootstrap for the layout and DayJs for the current date.
+
+
+
